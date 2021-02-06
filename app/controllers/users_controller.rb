10eb5d0a1440280c11a9afbb387fb1468264b1ab
rgollila <rgollila@gmail.com>
2021-02-06 11:39:58 -0500

    added support for deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 2)
+    @users = User.paginate(page: params[:page], per_page: 4)
   end
 
   def new
@@ -38,6 +38,13 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles deleted"
+    redirect_to articles_path
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  has_many :articles
+  # specifying dependent: :destroy will cause all articles of a given
+  # user to be destroyed if the user is destroyed.
+  has_many :articles, dependent: :destroy
   validates :username, presence: true, length: { minimum: 3, maximum: 25 },
             uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
